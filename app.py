bf17e73e96ec469fb564a1fd3abcfbcff8b8b42d
mecinas <szymon.mecina@vp.pl>
2021-11-02 18:51:10 +0100

    new

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def home_view():
-        return "<h1>Welcome to Geeks for Geekss</h1>"
+        return "<h1>Welcome to Geeks for Geekss sa</h1>"
  
 if __name__ == "__main__":
         app.run()
