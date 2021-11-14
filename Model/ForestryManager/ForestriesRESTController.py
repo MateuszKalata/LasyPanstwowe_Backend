d846579892b7c31708b6dba07feee93ae75d63fe
Marcin <idowerr@gmail.com>
2021-11-14 01:50:57 +0100

    jsonify

--- Model/ForestryManager/ForestriesRESTController.py
+++ Model/ForestryManager/ForestriesRESTController.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, request
 from Model.ForestryManager.ForestriesImpl import ForestriesImpl
 from DTO.XForestry import XForestry
-from DTO.XForestryDetails import XForestryDetails
 from DTO.XForestArea import XForestArea
-import json
+from flask import jsonify
 
 forestries_controller = Blueprint('ForestriesRESTController', __name__)
 forestries = ForestriesImpl()
@@ -14,7 +13,7 @@ def get_forest_areas(id):
     xforestrareas = forestries.get_forest_areas(int(id))
     if xforestrareas == 1:
         return "not found", 404
-    return json.dumps([xforestarea.as_dict() for xforestarea in xforestrareas]), 200
+    return jsonify([xforestarea.as_dict() for xforestarea in xforestrareas]), 200
 
 
 @forestries_controller.route("/forestareas", methods=['POST'])
@@ -45,7 +44,7 @@ def post_forestry():
 def send_forestries():
     xforestries = forestries.get_forestries()
     xforestries = [xforestry.as_dict() for xforestry in xforestries]
-    return json.dumps(xforestries), 200
+    return jsonify(xforestries), 200
 
 
 @forestries_controller.route("/forestry/<id>", methods=['GET'])
@@ -53,4 +52,4 @@ def send_forestry(id):
     xforestry = forestries.get_forestry(id)
     if xforestry == 1:
         return "not found", 404
-    return json.dumps(xforestry.as_dict()), 200
+    return jsonify(xforestry.as_dict()), 200
