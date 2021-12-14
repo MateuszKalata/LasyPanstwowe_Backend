75c8367c4ff8fed7b8b4c87f6953296d0fbc09ab
marcin <marcin@example.com>
2021-12-14 22:50:11 +0100

    emergency notification spaghetti fix check sensor id

--- Model/EmergencyNotificationManager/EmergencyNotificationsImpl.py
+++ Model/EmergencyNotificationManager/EmergencyNotificationsImpl.py
@@ -1,3 +1,4 @@
+from Data.SensorRepository.SensorRepositoryImpl import SensorRepositoryImpl
 from Model.EmergencyNotificationManager.IEmergencyNotification import IEmergencyNotification
 from Model.EmergencyNotificationManager.IEmergencyReporting import IEmergencyReporting
 from Data.EmergencyNotificationRepository.EmergencyNotificationRepositoryImpl import EmergencyNotificationRepositoryImpl
@@ -8,6 +9,7 @@ class EmergencyNotificationImpl(IEmergencyNotification, IEmergencyReporting):
 
     def __init__(self):
         self.emergency_notification_repository = EmergencyNotificationRepositoryImpl()
+        self.sensor_repository = SensorRepositoryImpl()
 
     def get_emergency_notification(self, id):
         return self.emergency_notification_repository.read(id)
@@ -19,5 +21,7 @@ class EmergencyNotificationImpl(IEmergencyNotification, IEmergencyReporting):
         self.emergency_notification_repository.update(id, {"status": 0})
 
     def report_emergency(self, x_emergency_notification_details):
+        if self.sensor_repository.read(x_emergency_notification_details.sensor_id) == 1:
+            raise APIException(f"Sensor with id={x_emergency_notification_details.sensor_id} doesn't exist!", 422)
         return self.emergency_notification_repository.create(x_emergency_notification_details)
 
